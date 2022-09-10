410db66d33674f7d8b05bdeb7c82b07a7914499d
Lanre Akintayo <akintayolanre2019@gmail.com>
2022-09-10 17:50:22 +0100

    Updated

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    NEXT_PUBLIC_PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
+    NEXT_PUBLIC_API_SECRET_KEY: process.env.NEXT_PUBLIC_API_SECRET_KEY,
+    NEXT_PUBLIC_IPFS_API_ENDPOINT: process.env.NEXT_PUBLIC_IPFS_API_ENDPOINT,
+    NEXT_PUBLIC_PRIVATE_KEY: proces.env.NEXT_PUBLIC_PRIVATE_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
