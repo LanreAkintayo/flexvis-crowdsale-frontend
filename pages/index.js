119c321577e1820fa1a86380cc0ab2c9addca6c5
Lanre Akintayo <akintayolanre2019@gmail.com>
2022-09-10 23:02:58 +0100

    Updated

--- pages/index.js
+++ pages/index.js
@@ -32,58 +32,9 @@ export default function Home() {
       <section>
         <Header />
 
-        <div className="relative w-full h-screen">
-          <div className="absolute w-full h-full bg-gradient-to-r dark:from-black from-black to-gray-600 dark:to-gray-700">
-            <div className="absolute w-full">
-              <img
-                alt="..."
-                src="/bg1.jpg"
-                className="object-cover w-full h-screen rounded-md opacity-1 mix-blend-overlay"
-              />
-            </div>
-          </div>
-          <div className="mx-7 absolute h-4/6 flex flex-col justify-center text-white">
-            <div className="text-white text-3xl w-12/12 font-medium sm:text-4xl lg:text-5xl w-full lg:w-8/12 pr-2">
-              <p className="w-full">
-                Best Crowdfund Platform for Personal Projects
-              </p>
-            </div>
-            <p className="my-4 text-xl">Fund with varieties of tokens</p>
-            <div className="flex text-base ss:text-xl mt-6">
-              <button
-                className="bg-green-800 p-2 rounded-md"
-                onClick={() => {
-                  if (isWeb3Enabled) {
-                    window.open("/launch", "_self");
-                  } else {
-                    window.alert("Connect your wallet");
-                  }
-                }}
-              >
-                Get Funded
-              </button>
-
-              <button
-                className="border border-green-500 p-2 rounded-md ml-4 text-green-500"
-                onClick={() => {
-                  if (isWeb3Enabled) {
-                    window.open("/projects", "_self");
-                  } else {
-                    window.alert("Connect your wallet");
-                  }
-                }}
-              >
-                Browse Project
-              </button>
-            </div>
-          </div>
-        </div>
+        
       </section>
-      <section>
-        <h1 className="text-3xl mt-10 px-5">Explore Projects</h1>
-        <ProjectCardSection />
-      </section>
-      <Footer />
+  
     </div>
   );
 }
